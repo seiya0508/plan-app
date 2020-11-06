a780c1c23612193e2b6cedf60c06655bd4dde90f
seiya0508 <71762860+seiya0508@users.noreply.github.com>
2020-11-06 15:03:50 +0900

    バリデーション機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :garbages
+
+  with_options presence: true do
+    validates :nick_kname
+    validates :full_name
+    validates :email, format: { with: /\A\S+@\S+\.\S+\z/ }
+    validates :password, format: { with: /\A[a-z0-9]+\z/ }
+  end
 end
