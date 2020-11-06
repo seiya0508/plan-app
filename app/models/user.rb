e2e4830cc344755ee2d3bc519b4d181b67426221
seiya0508 <71762860+seiya0508@users.noreply.github.com>
2020-11-06 16:30:18 +0900

    ログインページエラー表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :garbages
 
   with_options presence: true do
-    validates :nick_kname
+    validates :nick_name
     validates :full_name
     validates :email, format: { with: /\A\S+@\S+\.\S+\z/ }
     validates :password, format: { with: /\A[a-z0-9]+\z/ }
