10087df8f21c6f30c2973ea838e7ece6dd8223db
seiya0508 <71762860+seiya0508@users.noreply.github.com>
2020-11-06 12:50:39 +0900

    Rubocooの導入

--- test/controllers/garbages_controller_test.rb
+++ test/controllers/garbages_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class GarbagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get garbages_index_url
     assert_response :success
   end
-
 end
