7422a471f7f1e47ebd1cba15aacbfbb94d65b622
seiya0508 <71762860+seiya0508@users.noreply.github.com>
2020-11-19 10:38:11 +0900

    garbageモデル単体テスト

--- spec/models/garbage_spec.rb
+++ spec/models/garbage_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Garbage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @garbage = FactoryBot.build(:garbage)
+  end
+  describe '予定の新規登録' do
+    context '新規登録ができる時' do
+      it 'dow,title,が存在すれば登録できる' do
+        expect(@garbage).to be_valid
+      end
+
+    context '新規登録がうまくいかないとき' do
+      it 'dowが空だと登録できない' do
+        @garbage.dow = nil
+        @garbage.valid?
+        expect(@garbage.errors.full_messages).to include("Dowを入力してください")
+      end
+    
+      it 'titleが空だと登録できない' do
+        @garbage.title = nil
+        @garbage.valid?
+        expect(@garbage.errors.full_messages).to include("Titleを入力してください")
+      end
+
+     end
+    end
+  end
 end
